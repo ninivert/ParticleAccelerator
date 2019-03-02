6efedf321633f6ee011a3d19df2ccfb5b05a9a7a
Niels Vadot <ninivert1@gmail.com>
2019-03-02 19:21:36 +0100

    changed naming conventions

--- src/lib/Vector3D.cpp
+++ src/lib/Vector3D.cpp
@@ -3,82 +3,103 @@
 using namespace std;
 using namespace CONSTANTS;
 
+/****************************************************************
+ * Constructor
+ ****************************************************************/
 
-/**
- * Vector3D
- */
-
-Vector3D::Vector3D(const double& _x, const double& _y, const double& _z) {
+Vector3D::Vector3D(double const& _x, double const& _y, double const& _z) {
 	x = _x;
 	y = _y;
 	z = _z;
 }
 
-Vector3D::Vector3D(const Vector3D& v) {
+Vector3D::Vector3D(Vector3D const& v) {
 	x = v.getX();
 	y = v.getY();
 	z = v.getZ();
 }
-// Getters
+
+/****************************************************************
+ * Getters
+ ****************************************************************/
+
 double Vector3D::getX() const { return x; }
 double Vector3D::getY() const { return y; }
 double Vector3D::getZ() const { return z; }
 
-// Setters
-void Vector3D::setX(const double& _x) { x = _x; }
-void Vector3D::setY(const double& _y) { y = _y; }
-void Vector3D::setZ(const double& _z) { z = _z; }
+/****************************************************************
+ * Setters
+ ****************************************************************/
 
-// Overloading
-Vector3D Vector3D::operator + (const Vector3D& v) const {
+void Vector3D::setX(double const& _x) { x = _x; }
+void Vector3D::setY(double const& _y) { y = _y; }
+void Vector3D::setZ(double const& _z) { z = _z; }
+
+/****************************************************************
+ * Overloading
+ ****************************************************************/
+
+Vector3D Vector3D::operator + (Vector3D const& v) const {
 	Vector3D res(*this);
 	res.__add__(v);
 	return res;
 }
 
-Vector3D Vector3D::operator - (const Vector3D& v) const {
+Vector3D Vector3D::operator - (Vector3D const& v) const {
 	Vector3D res(*this);
 	res.__sub__(v);
 	return res;
 }
 
-Vector3D Vector3D::operator * (const double& lambda) const {
+Vector3D Vector3D::operator * (double const& lambda) const {
 	Vector3D res(*this);
 	res.__mult__(lambda);
 	return res;
 }
-Vector3D Vector3D::operator / (const double& lambda) const {
+
+Vector3D Vector3D::operator / (double const& lambda) const {
 	Vector3D res(*this);
 	res.__div__(lambda);
 	return res;
 }
-Vector3D Vector3D::operator ^ (const Vector3D& v) const {
+
+Vector3D Vector3D::operator ^ (Vector3D const& v) const {
 	Vector3D res(*this);
 	res.__cross__(v);
 	return res;
 }
-double Vector3D::operator * (const Vector3D& v) const {
+
+double Vector3D::operator * (Vector3D const& v) const {
 	Vector3D res(*this);
 	return res.__dot__(v);
 }
-bool Vector3D::operator == (const Vector3D& v) const {
+
+bool Vector3D::operator == (Vector3D const& v) const {
 	return __eq__(v);
 }
-void Vector3D::operator = (const Vector3D& v) {
+
+void Vector3D::operator = (Vector3D const& v) {
 	x = v.getX();
 	y = v.getY();
 	z = v.getZ();
 }
-// Methods
+
+/****************************************************************
+ * Methods
+ ****************************************************************/
+
 string Vector3D::stringify() const {
-return "("s + to_string(x) + ", "s + to_string(y) + ", "s + to_string(z) + ")"s;
+	return "("s + to_string(x) + ", "s + to_string(y) + ", "s + to_string(z) + ")"s;
 }
+
 double Vector3D::norm() const {
 	return sqrt(normSquared());
 }
+
 double Vector3D::normSquared() const {
 	return pow(x, 2) + pow(y, 2) + pow(z, 2);
 }
+
 void Vector3D::normalize() {
 	double n = norm();
 	x /= n;
@@ -86,33 +107,43 @@ void Vector3D::normalize() {
 	z /= n;
 }
 
-// Static methods
-double Vector3D::tripleProduct(const Vector3D& v1, const Vector3D& v2, const Vector3D& v3) {
+/****************************************************************
+ * Static methods
+ ****************************************************************/
+
+double Vector3D::tripleProduct(Vector3D const& v1, Vector3D const& v2, Vector3D const& v3) {
 	return v1 * (v2 ^ v3);
 }
 
-// Overloading methods
-void Vector3D::__add__(const Vector3D& v) {
+/****************************************************************
+ * Overloding methods
+ ****************************************************************/
+
+void Vector3D::__add__(Vector3D const& v) {
 	x += v.getX();
 	y += v.getY();
 	z += v.getZ();
 }
-void Vector3D::__sub__(const Vector3D& v) {
+
+void Vector3D::__sub__(Vector3D const& v) {
 	x -= v.getX();
 	y -= v.getY();
 	z -= v.getZ();
 }
-void Vector3D::__mult__(const double& lambda) {
+
+void Vector3D::__mult__(double const& lambda) {
 	x *= lambda;
 	y *= lambda;
 	z *= lambda;
 }
-void Vector3D::__div__(const double& lambda) {
+
+void Vector3D::__div__(double const& lambda) {
 	x /= lambda;
 	y /= lambda;
 	z /= lambda;
 }
-void Vector3D::__cross__(const Vector3D& v) {
+
+void Vector3D::__cross__(Vector3D const& v) {
 	double _x = y*v.getZ() - z*v.getY();
 	double _y = z*v.getX() - x*v.getZ();
 	double _z = x*v.getY() - y*v.getX();
@@ -120,23 +151,23 @@ void Vector3D::__cross__(const Vector3D& v) {
 	y = _y;
 	z = _z;
 }
-double Vector3D::__dot__(const Vector3D& v) const {
+
+double Vector3D::__dot__(Vector3D const& v) const {
 	return x*v.getX() + y*v.getY() + z*v.getZ();
 }
-bool Vector3D::__eq__(const Vector3D& v) const {
+
+bool Vector3D::__eq__(Vector3D const& v) const {
 	bool a = abs(x - v.getX()) < EPSILON;
 	bool b = abs(y - v.getY()) < EPSILON;
 	bool c = abs(z - v.getZ()) < EPSILON;
 	return a and b and c;
 }
 
-/**
+/****************************************************************
  * Cout overloading
- */
+ ****************************************************************/
 
-ostream& operator<< (ostream& stream, const Vector3D& v) {
-	string x = v.stringify();
-	stream << x;
+ostream& operator<< (ostream& stream, Vector3D const& v) {
+	stream << v.stringify();
 	return stream;
 }
-
--- test/src/Vector3D.cpp
+++ test/src/Vector3D.cpp
@@ -5,103 +5,99 @@
 
 using namespace std;
 
-/**
- * Main
- */
-
 int main() {
-	Vector3D v1(2, -5, 6);
-	Vector3D v2(3, -7, -2);
-	Vector3D v3(4, 7, -1);
-	Vector3D v4(-3, 3, -3);
-	Vector3D v5(4, -3, 0);
+	Vector3D v1(2, -5, 6); // no const here because we call v1.normalize() later on
+	Vector3D const v2(3, -7, -2);
+	Vector3D const v3(4, 7, -1);
+	Vector3D const v4(-3, 3, -3);
+	Vector3D const v5(4, -3, 0);
 
-	cout << v1 << endl;
-	cout << v2 << endl;
-	cout << v3 << endl;
+	// cout << v1 << endl;
+	// cout << v2 << endl;
+	// cout << v3 << endl;
 
-	cout << "\nARITHMETIC\n\n";
+	// cout << "\nARITHMETIC\n\n";
 
-	cout << "Operator +\n";
+	// cout << "Operator +\n";
 	assert((v1 + v2) == Vector3D(5, -12, 4));
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v1 + v2 << endl;
 
-	cout << "Operator -\n";
+	// cout << "Operator -\n";
 	assert((v1 - v2) == Vector3D(-1, 2, 8));
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v1 - v2 << endl;
 
-	cout << "Operator * (scalar)\n";
+	// cout << "Operator * (scalar)\n";
 	assert((v1 * 2) == Vector3D(4, -10, 12));
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v1 * 2 << endl;
 
-	cout << "Operator / (scalar)\n";
+	// cout << "Operator / (scalar)\n";
 	// assert((v1 / 3) == Vector3D(0.666667, -1.666667, 2.000000));
 		// cout << "\tpassed\n";
-		cout << "\t";
-		cout << v1 / 3 << endl;
+		// cout << "\t";
+		// cout << v1 / 3 << endl;
 
-	cout << "Operator * (Vector3D:dot product)\n";
+	// cout << "Operator * (Vector3D:dot product)\n";
 	assert((v1 * v2) == 29);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v1 * v2 << endl;
 
-	cout << "Operator ^ (Vector3D::cross product)\n";
+	// cout << "Operator ^ (Vector3D::cross product)\n";
 	assert((v1 ^ v2) == Vector3D(52, 22, 1));
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << (v1 ^ v2) << endl;
 	assert((v1 ^ v2) * v2 == 0);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << (v1 ^ v2) * v2 << endl;
 	assert((v1 ^ v2) * v1 == 0);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << (v1 ^ v2) * v1 << endl;
 
-	cout << "Triple Product\n";
+	// cout << "Triple Product\n";
 	assert(Vector3D::tripleProduct(v1, v2, v3) == 361);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << Vector3D::tripleProduct(v1, v2, v3) << endl;
 
-	cout << "Norm\n";
+	// cout << "Norm\n";
 	assert(v5.norm() == 5);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v5.norm() << endl;
 
-	cout << "NormSquared\n";
+	// cout << "NormSquared\n";
 	assert(v1.normSquared() == 65);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << v1.normSquared() << endl;
 
-	cout << "\nCOMPARAISON\n\n";
+	// cout << "\nCOMPARAISON\n\n";
 
-	cout << "Operator == (Vector3D: equality test)\n";
+	// cout << "Operator == (Vector3D: equality test)\n";
 	assert((v1 == v1) == 1);
 		// cout << (v1 == v1) << endl;
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 	assert((v1 == v2) == 0);
 		// cout << (v1 == v2) << endl;
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 
-	cout << "\nASSIGNMENT\n\n";
+	// cout << "\nASSIGNMENT\n\n";
 
-	cout << "Operator = (Vector3D: assignement test)\n";
+	// cout << "Operator = (Vector3D: assignement test)\n";
 	v1 = v2;
 	assert((v1 == v2) == 1);
-		cout << "\tpassed\n";
+		// cout << "\tpassed\n";
 		// cout << (v1 == v2) << endl;
 
-	cout << "\nNORMALIZATION\n\n";
-	cout << "Normalization\n";
+	// cout << "\nNORMALIZATION\n\n";
+	// cout << "Normalization\n";
 	v1 = v5;
 	v1.normalize();
 	assert(Test::eq((v1.norm()),1));
-		cout << "\tpas...\n";
+		// cout << "\tpas...\n";
 	assert((v1 * v5) == v5.norm());
-		cout << "\t...sed\n";
+		// cout << "\t...sed\n";
 		// cout << v1 << endl;
 
-	cout << "\nPASSED ALL TESTS.\n";
-
+	// cout << "\nPASSED ALL TESTS.\n";
+	cout << "[Vector3D]: passed all tests." << endl;
 }
