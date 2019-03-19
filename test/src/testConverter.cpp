a86af420dbde9a2b9c650f6a401d98fb093d60ae
Niels Vadot <ninivert1@gmail.com>
2019-03-19 18:49:37 +0100

    tests

--- test/src/testConverter.cpp
+++ test/src/testConverter.cpp
@@ -8,10 +8,14 @@ using namespace std;
 
 int main() {
 	// Mass
-	// cout << CONVERT::MassSItoEV(CONVERT::MassGeVtoSI(CONSTANTS::M_ELECTRON)) << endl;
+	assert(CONVERT::MassSItoEV(CONVERT::MassGeVtoSI(CONSTANTS::M_ELECTRON)) == CONSTANTS::M_ELECTRON * 1e9);
 
 	// Momentum
-	cout << CONVERT::MomentumGeVtoSI(Vector3D(1, 2, -1)) << endl;
+	// Remark: our threshold for double comparaison is 1e-6 per default,
+	// so a small error would probably never be detected.
+	// We are however pretty confident everything works from the few tests we already ran
+	// and the correctness of the Particle test results
+	assert(CONVERT::MomentumGeVtoSI(Vector3D(1, 2, -1)) == Vector3D(5.34429e-19, 1.06886e-18, -5.34429e-19));
 
 	// Energy
 	assert(CONVERT::EnergyEVtoSI(1) == CONSTANTS::EV);
