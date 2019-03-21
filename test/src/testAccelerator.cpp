d90c331ca26a53efd5057782fa688f05dff0a92c
HugoArbez <hugo.arbez@gmail.com>
2019-03-21 16:54:37 +0100

    Fixed dependencies of Accelerator

--- test/src/testAccelerator.cpp
+++ test/src/testAccelerator.cpp
@@ -4,8 +4,8 @@
 #include "src/include/Vector3D.h"
 #include "src/include/Particle.h"
 #include "src/include/Element.h"
-#include "src/include/MagnetElement.h"
-#include "src/include/ElectrElement.h"
+#include "src/include/Dipole.h"
+#include "src/include/Quadrupole.h"
 #include "src/include/Accelerator.h"
 #include "test/lib/Test.h"
 
