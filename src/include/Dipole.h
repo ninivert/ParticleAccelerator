7758960a9f40ab762839d370a890454056ce4008
HugoArbez <hugo.arbez@gmail.com>
2019-03-21 16:42:49 +0100

    Avec le commit c'est mieux

---
+++ src/include/Dipole.h
@@ -0,0 +1,26 @@
+#ifndef DIPOLE_H
+#define DIPOLE_H
+
+#pragma once
+
+#include "src/include/Element.h"
+#include "src/include/Particle.h"
+#include "src/include/Vector3D.h"
+#include "src/globals.h"
+
+/**
+ * Dipole
+ */
+
+class Dipole : public Element {
+public:
+
+protected:
+
+private:
+
+
+};
+
+
+#endif
---
+++ src/include/Quadrupole.h
@@ -0,0 +1,26 @@
+#ifndef QUADRUPOLE_H
+#define QUADRUPOLE_H
+
+#pragma once
+
+#include "src/include/Element.h"
+#include "src/include/Particle.h"
+#include "src/include/Vector3D.h"
+#include "src/globals.h"
+
+/**
+ *Quadrupole
+ */
+
+class Quadrupole : public Element {
+public:
+
+protected:
+
+private:
+
+
+};
+
+
+#endif
