02bf76488e9ba3dc785d46a3c530b6d936d36a48
HugoArbez <hugo.arbez@gmail.com>
2019-03-21 22:00:14 +0100

    implementation of a part of Element class

--- src/include/Element.h
+++ src/include/Element.h
@@ -3,7 +3,9 @@
 
 #pragma once
 
+#include <iomanip>
 #include <string>
+#include <sstream>
 
 #include "src/include/Vector3D.h"
 #include "src/include/Particle.h"
@@ -24,19 +26,37 @@ public:
 	// WARNING: prev and next need to stay coherent
 	Element(Vector3D const& posIn, Vector3D const& posOut, double const& radius, Element & prev);
 
+	// Getters
+
+	Vector3D getPosIn() const;
+	Vector3D getPosOut() const;
+	double getRadius() const;
+
+	// Setters
+
+	void setPrev(Element * prev);
+	void setNext(Element * next);
+
 	// Methods
+
+	void pointToNext(Element & next);
+	std::string to_string() const;
+
+	// Virtual methods
 
 	virtual Vector3D getField() const = 0;
 	virtual bool isInWall(Particle const& p) const = 0;
 	virtual bool isInNext(Particle const& p) const = 0;
-	virtual std::string to_string() const = 0;
+	// overloading of to_string function in order to allow the stringification of an Element
+	// virtual std::string to_string() const = 0;
+
 
 protected:
 	Vector3D const posIn;
 	Vector3D const posOut;
 	double const radius;
-	Element * next = nullptr;
-	Element * prev = nullptr;
+	Element * next;		// initialised to nullptr
+	Element * prev;		// initialised to nullptr
 };
 
 /**
