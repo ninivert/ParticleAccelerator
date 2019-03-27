536470708501c0f533049538760feda79d28850e
Niels Vadot <ninivert1@gmail.com>
2019-03-28 00:20:12 +0100

    removed unnecessary dependencies, added Drawable and Renderer includes

--- src/include/Accelerator.h
+++ src/include/Accelerator.h
@@ -20,9 +20,8 @@
 #include "src/include/Vector3D.h"
 #include "src/include/Particle.h"
 #include "src/include/Element.h"
-#include "src/include/Dipole.h"
-#include "src/include/Quadrupole.h"
-#include "src/include/Straight.h"
+#include "src/include/Drawable.h"
+#include "src/include/Renderer.h"
 #include "src/globals.h"
 #include "src/exceptions.h"
 
