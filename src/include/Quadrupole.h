078ea22f82f1f0bd1182e2538a51b063b51398b5
Niels Vadot <ninivert1@gmail.com>
2019-04-05 15:29:01 +0200

    remOWOve "to be DELETED"

--- src/include/Quadrupole.h
+++ src/include/Quadrupole.h
@@ -68,7 +68,7 @@ public:
 	 ****************************************************************/
 
 	/**
-	 * Returns a new shared_ptr constructed dynamically (to be DELETED).
+	 * Returns a new shared_ptr constructed dynamically
 	 *
 	 * This new pointer contains a polymorphic copy of the current Quadrupole element.
 	 *
