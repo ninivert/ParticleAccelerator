fb6e02806bb10e10298efa79d64c0db1c807384c
Niels Vadot <ninivert1@gmail.com>
2019-05-21 12:55:15 +0200

    did an oopsie

--- common/lib/Window.cpp
+++ common/lib/Window.cpp
@@ -4,7 +4,7 @@
  * General stuffs
  ****************************************************************/
 
-Window::Window() : focus(true), acc(&engine, true, false), frames(0), engineSpeed(0) {
+Window::Window() : focus(true), acc(&engine, true, false), frames(0), engineSpeed(1) {
 	// Cursor
 	QCursor c;
 	c.setPos(mapToGlobal(QPoint(width() / 2, height() / 2)));
